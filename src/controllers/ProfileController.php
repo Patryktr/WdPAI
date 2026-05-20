495a5ad9685455192e90dc324b7842799fc050d4
patryktr <patryk.trojan@onet.pl>
2026-05-21 00:09:02 +0200

    Add profile account view

--- src/controllers/ProfileController.php
+++ src/controllers/ProfileController.php
@@ -1,6 +1,7 @@
 <?php
 
 require_once 'AppController.php';
+require_once __DIR__.'/../repositories/UsersRepository.php';
 
 class ProfileController extends AppController {
 
@@ -8,6 +9,17 @@ class ProfileController extends AppController {
     {
         $this->requireLogin();
 
-        $this->render("profile", ["title" => "Profile"]);
+        $usersRepository = new UsersRepository();
+        $user = $usersRepository->getUserById($this->currentUserId());
+
+        if ($user === null) {
+            $this->setFlash('error', 'Nie znaleziono profilu użytkownika.');
+            $this->redirect('/logout');
+        }
+
+        $this->render("profile", [
+            "title" => "Profile",
+            "user" => $user,
+        ]);
     }
 }
